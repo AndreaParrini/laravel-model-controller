6e487edb4c1c1fabec2192204234f278bb0aabfe
AndreaParrini <andrea.parrini26@gmail.com>
2024-04-16 22:46:41 +0200

    modify welcome and create new view movies

---
+++ resources/views/movies.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.layout')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        @foreach ($movies as $movie)
+        <div class="col-4">
+            <div class="card">
+                <div class="title">
+                    <h3>{{$movie['title']}}</h3>
+                </div>
+                <div class="original-title">
+                    Original Title: {{$movie['original_title']}}
+                </div>
+                <div class="nationality">
+                    Nation: {{$movie['nationality']}}
+                </div>
+                <div class="date">
+                    Date: {{$movie['date']}}
+                </div>
+                <div class="vote">
+                    Vote: {{$movie['vote']}}
+                </div>
+            </div>
+        </div>            
+        @endforeach
+    </div>
+</div>
+@endsection
