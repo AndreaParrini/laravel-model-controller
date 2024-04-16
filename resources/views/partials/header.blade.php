d9e06b1b9189ea457cc8d4f055865c8f93e6a870
AndreaParrini <andrea.parrini26@gmail.com>
2024-04-16 22:47:28 +0200

    add style of welcome and movies view modify file header insert a in a li tag

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,8 +3,8 @@
         <nav>
            <h3>Model-Controller</h3>
             <ul>
-                <li>Home</li>
-                <li>Movies</li>
+                <li><a href="{{Route('home')}}">Home</a></li>
+                <li><a href="{{Route('movies')}}">Movies</a></li>
                 <li>About</li>
             </ul> 
         </nav>
