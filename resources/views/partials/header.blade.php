b15688951b1e152d7db961c4491a88725741137a
AndreaParrini <andrea.parrini26@gmail.com>
2024-04-16 22:38:06 +0200

    add layout and partials
    add header and footer
    add style in css

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="container">
+        <nav>
+           <h3>Model-Controller</h3>
+            <ul>
+                <li>Home</li>
+                <li>Movies</li>
+                <li>About</li>
+            </ul> 
+        </nav>
+        
+    </div>
+</header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,57 +1,29 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout.layout')
 
-        <title>Model-Controller</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="antialiased">
-        <header>
-            <div class="container">
-                <nav>
-                   <h3>Model-Controller</h3>
-                    <ul>
-                        <li>Home</li>
-                        <li>Movies</li>
-                        <li>About</li>
-                    </ul> 
-                </nav>
-                
+@section('content')
+<div class="container">
+    <div class="row">
+        @foreach ($movies as $movie)
+        <div class="col-4">
+            <div class="card">
+                <div class="title">
+                    <h3>{{$movie['title']}}</h3>
+                </div>
+                <div class="original-title">
+                    Original Title: {{$movie['original_title']}}
+                </div>
+                <div class="nationality">
+                    Nation: {{$movie['nationality']}}
+                </div>
+                <div class="date">
+                    Date: {{$movie['date']}}
+                </div>
+                <div class="vote">
+                    Vote: {{$movie['vote']}}
+                </div>
             </div>
-        </header>
-        <div class="container">
-            <div class="row">
-                @foreach ($movies as $movie)
-                <div class="col-4">
-                    <div class="card">
-                        <div class="title">
-                            <h3>{{$movie['title']}}</h3>
-                        </div>
-                        <div class="original-title">
-                            Original Title: {{$movie['original_title']}}
-                        </div>
-                        <div class="nationality">
-                            Nation: {{$movie['nationality']}}
-                        </div>
-                        <div class="date">
-                            Date: {{$movie['date']}}
-                        </div>
-                        <div class="vote">
-                            Vote: {{$movie['vote']}}
-                        </div>
-                    </div>
-                </div>            
-                @endforeach
-            </div>
-        </div>
-        
-    </body>
-</html>
+        </div>            
+        @endforeach
+    </div>
+</div>
+@endsection
